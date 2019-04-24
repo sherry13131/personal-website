c11dcfa27d13ac511835e2b611de949bf0b0f1ad
Sherry Ma <sherry.hyma@gmail.com>
2019-04-24 00:16:49 -0400

    working on project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -10,24 +10,8 @@ export class Projects extends Component {
 
     render() {
         return (
-            <div className="main-cont">
-                <div className="projects-top">
-                    <h1>Projects</h1>
-                    <p>Here are some of the project I have been working on recently:</p>
-                    <div className="project-btns">
-                        <ul>
-                            <li>CatchEm* web app</li>
-                            <li>Home database library</li>
-                            <li>Minesweeper</li>
-                            <li>Android online bank system</li>
-                            <li>And of course this website !!!</li>
-                            <li>And more on github !</li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="project-bottom">
-                
-                </div>
+            <div>
+                Still working on it...
             </div>
         );
     }
