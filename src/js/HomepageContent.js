7ca7ed6cbc5902d1049ba62093114b5fc790c4bb
Sherry Ma <sherry.hyma@gmail.com>
2019-10-15 13:08:59 -0400

    add bubbles on background

--- src/js/HomepageContent.js
+++ src/js/HomepageContent.js
@@ -1,8 +1,9 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
-import React, { Component } from 'react';
-import { NavLink, withRouter } from "react-router-dom";
+import React, { Component, Fragment } from 'react';
+import { withRouter } from "react-router-dom";
 import Typing from 'react-typing-animation';
 import Emoji from 'a11y-react-emoji'
+import Particles from 'react-particles-js';
 import '../styles/homepage.css';
 
 class HomepageContent extends Component {
@@ -54,30 +55,86 @@ class HomepageContent extends Component {
     render() {
         let { showThis } = this.state;
         return (
-            <div className="hp-content-cont noselect">
-                <Typing speed={50} className="auto-type" loop={true} onFinishedTyping={this.randomsentence}>
-                    <span>{ showThis }</span>
-                    <Typing.Backspace count={showThis.length} delay={5000}/>
-                    <Typing.Delay ms={2000} />
-                </Typing>
-                <div className="hp-content">
-                    <div className="hp-left">
-                        <div className="myName">Sherry Ma</div>
-                        <div className="content" id="title">Student at University of Toronto Scarborough</div>
-                        <div className="content" id="title">Software Developer</div>
-                        <div className="social-btns-cont">
-                            <a className="social-btn" id="linkedin" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/sherry-hyma/"></a>
-                            <a className="social-btn" id="github" target="_blank" rel="noopener noreferrer" href="https://github.com/sherry13131"></a>
-                            <a className="social-btn" id="mail" target="_blank" rel="noopener noreferrer" href="mailto:sherry.hyma@gmail.com"></a>
+            <Fragment>
+<Particles
+    style={{"position": "absolute"}}
+    params={{
+	    "particles": {
+	        "number": {
+	            "value": 160,
+	            "density": {
+	                "enable": false
+	            }
+	        },
+	        "size": {
+	            "value": 3,
+	            "random": true,
+	            "anim": {
+	                "speed": 4,
+	                "size_min": 0.3
+	            }
+	        },
+	        "line_linked": {
+	            "enable": false
+	        },
+	        "move": {
+	            "random": true,
+	            "speed": 1,
+	            "direction": "top",
+	            "out_mode": "out"
+	        }
+	    },
+	    "interactivity": {
+	        "events": {
+	            "onhover": {
+	                "enable": true,
+	                "mode": "bubble"
+	            },
+	            "onclick": {
+	                "enable": true,
+	                "mode": "repulse"
+	            }
+	        },
+	        "modes": {
+	            "bubble": {
+	                "distance": 250,
+	                "duration": 2,
+	                "size": 0,
+	                "opacity": 0
+	            },
+	            "repulse": {
+	                "distance": 400,
+	                "duration": 4
+	            }
+	        }
+	    }
+	}} />
+
+                <div className="hp-content-cont noselect">
+                    <Typing speed={50} className="auto-type" loop={true} onFinishedTyping={this.randomsentence}>
+                        <span>{ showThis }</span>
+                        <Typing.Backspace count={showThis.length} delay={5000}/>
+                        <Typing.Delay ms={2000} />
+                    </Typing>
+                    <div className="hp-content">
+                        <div className="hp-left">
+                            <div className="myName">Sherry Ma</div>
+                            <div className="content" id="title">Student at University of Toronto Scarborough</div>
+                            <div className="content" id="title">Software Developer</div>
+                            <div className="social-btns-cont">
+                                <a className="social-btn" id="linkedin" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/sherry-hyma/"></a>
+                                <a className="social-btn" id="github" target="_blank" rel="noopener noreferrer" href="https://github.com/sherry13131"></a>
+                                <a className="social-btn" id="mail" target="_blank" rel="noopener noreferrer" href="mailto:sherry.hyma@gmail.com"></a>
+                            </div>
+                        </div>
+                        <div className="hp-right">
+                            <div className="description">A student that is willing to learn and explore the world <Emoji symbol="🌏" label="earth" /></div>
+                            <div className="description">Love to keep up on new technologies and accept challenges.</div>
+                            <div className="description">A cat lover <Emoji symbol="🐱" label="cat" /> and sports enthusiast.</div>
                         </div>
                     </div>
-                    <div className="hp-right">
-                        <div className="description">A student that is willing to learn and explore the world <Emoji symbol="🌏" label="earth" /></div>
-                        <div className="description">Love to keep up on new technologies and accept challenges.</div>
-                        <div className="description">A cat lover <Emoji symbol="🐱" label="cat" /> and sports enthusiast.</div>
-                    </div>
                 </div>
-            </div>
+            </Fragment>
         );
     }
 }
