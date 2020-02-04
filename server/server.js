d466e787393322ad9399d2638501882d4cce05c9
Sherry Ma <sherry.hyma@gmail.com>
2020-02-03 21:37:21 -0500

    added send message feature, it will send to my email

--- server/server.js
+++ server/server.js
@@ -1,10 +1,32 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const cors = require("cors");
+const nodemailer = require('nodemailer');
+const creds = require('./config');
 
 const API_PORT = 3001;
 const app = express();
 const router = express.Router();
+
+var transport = {
+    host: 'smtp.gmail.com',
+    auth: {
+      user: creds.USER,
+      pass: creds.PASS
+    },
+    secure: true,
+    port: 465
+}
+
+var transporter = nodemailer.createTransport(transport);
+
+transporter.verify((err, success) => {
+    console.log("here");
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('All works!');
+    }
+});
 
 router.get('/sendform', function (req, res) {
     res.send('hello world');
@@ -12,8 +34,24 @@ router.get('/sendform', function (req, res) {
 
 router.post('/submit', function(req, res) {
     console.log(req.body);
-    res.send("hi2");
-})
+    const { name, email, message } = req.body;
+
+    let msg = message;
+    var mail = {
+        from: name,
+        to: 'sherry.hyma@gmail.com',
+        subject: 'A message from ' + name + '! (' + email + ')',
+        text: msg
+    }
+    console.log("created mail");
+    transporter.sendMail(mail, (err, data) => {
+        if (err) {
+            res.json({ msg: 'fail sending email' });
+        } else {
+            res.json({ msg: 'success' });
+        }
+    });
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
