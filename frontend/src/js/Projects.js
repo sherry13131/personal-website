eef07f4fb43752ddbd612cc19f67b40a8d2bc3d1
Sherry Ma <sherry.hyma@gmail.com>
2020-06-04 13:04:18 -0400

    Update Projects.js

--- frontend/src/js/Projects.js
+++ frontend/src/js/Projects.js
@@ -23,7 +23,7 @@ class Projects extends Component {
                     <div className="project-box-phone">
                         <div>
                             <h2><a className="project-btn" id="bank">Pokemon Discord Bot</a></h2>
-                            <h4 className="tech=p">Technologies: Node.js, MongoDB, Heroku</h4>
+                            <h4 className="tech=p">Technologies: Discord.js </h4>
                             <p>- A Discord game bot which allows users do catching and trading Pokemon with friends</p>
                             <p>- Implemented functionalities and interface design for catch, inventory, shop, and several other commands</p>
                             <p>- Deployed the application through Heroku for public use</p>
